45b8bac152728e9e819ac2d6bc056fce6f4ae19c
cafajardop <carlosafp84@hotmail.com>
2020-05-08 23:13:16 -0500

    prueba git hackerrank

--- InventoryWebApi/Program.cs
+++ InventoryWebApi/Program.cs
@@ -20,7 +20,7 @@ namespace InventoryWebApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
